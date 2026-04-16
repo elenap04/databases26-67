f2fc5859f5a2b929e1fc141d15f305ebc707c027
elenap04 <el.pappa04@gmail.com>
2026-04-16 16:59:36 +0300

    Add files via upload

---
+++ code/frontend/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path    = require('path');
+
+const app  = express();
+const PORT = 8080;
+
+app.use(express.static(path.join(__dirname)));
+
+app.get('/{*path}', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(PORT, () => console.log(`Frontend running on http://localhost:${PORT}`));
